925cc674fb0c0d55e5bc050f2781d3a53ec82445
Maxime Costalonga <maxime.costalonga@atos.net>
2023-04-02 20:29:14 +0200

    feat: add github copilot

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,21 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  {
+    -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
 }
