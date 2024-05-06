c51186c51c0f3fc6909d21d31d1753e658b76a3d
Maxime Costalonga <maxime.costalonga.external@atos.net>
2024-05-06 21:54:28 +0100

    augment timeout of formatter

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -36,7 +36,7 @@ return {
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
-      return { timeout_ms = 1000, lsp_fallback = true }
+      return { timeout_ms = 2000, lsp_fallback = true }
     end,
     -- Customize formatters
     formatters = {
