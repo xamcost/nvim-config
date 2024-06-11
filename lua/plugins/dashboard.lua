2266e4cee1700d0db9929c07dba20d7f126862b8
Maxime Costalonga <maxime.costalonga.external@atos.net>
2024-06-11 23:36:16 +0100

    feat: add ferris to dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -9,15 +9,16 @@ return {
          ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
          ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
          ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
-        　　　 　　／＞ 　フ   
-                  |   _  _|  
-        　 　　 　／` ミ_xノ   
-        　　 　 /  　　 　|    
-        　　　 /  ヽ　　 ﾉ    
-        　 　 │　   | | |     
-        　／￣|　　 | | |     
-        　| (￣ヽ＿_ヽ_)_)     
-        　＼二つ                
+
+        　　　 　　／＞ 　フ                                     
+                   |   _  _|                                     
+        　 　　 　／` ミ_xノ                                     
+        　　 　 /  　　 　|                                      
+        　　　 /  ヽ　　 ﾉ                 _~^~^~_               
+        　 　 │　   | | |              \) /  o o  \ (/           
+        　／￣|　　 | | |                '_   -   _'             
+        　| (￣ヽ＿_ヽ_)_)               / '-----' \             
+        　＼二つ                                                 
     ]]
 
     logo = string.rep("\n", 8) .. logo .. "\n\n"
@@ -33,15 +34,15 @@ return {
         header = vim.split(logo, "\n"),
         -- stylua: ignore
         center = {
-          { action = "Telescope find_files",                                    desc = " Find file",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+          { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
+          { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
+          { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
+          { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
           -- { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
-          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+          { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
+          { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
+          { action = "qa", desc = " Quit", icon = " ", key = "q" },
         },
         footer = function()
           local stats = require("lazy").stats()
