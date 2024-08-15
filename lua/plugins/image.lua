0b7355f4f85514348015ebc0381503dc8c42c6ca
Xamcost <maxime.costalonga@gmail.com>
2024-08-15 12:00:59 +0100

    fix(image): show images on cursor only
    to avoid sticking images when shifting between tmux panes (see
    https://github.com/3rd/image.nvim/issues/198 )

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -21,7 +21,7 @@ return {
           enabled = true,
           clear_in_insert_mode = false,
           download_remote_images = true,
-          only_render_image_at_cursor = false,
+          only_render_image_at_cursor = true,
           filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
         },
         neorg = {
@@ -45,8 +45,8 @@ return {
       max_height_window_percentage = 50,
       window_overlap_clear_enabled = false,                                               -- toggles images when windows are overlapped
       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-      editor_only_render_when_focused = false,                                            -- auto show/hide images when the editor gains/looses focus
-      tmux_show_only_in_active_window = false,                                            -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      editor_only_render_when_focused = true,                                             -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = true,                                             -- auto show/hide images in the correct Tmux window (needs visual-activity off)
       hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
     })
   end,
